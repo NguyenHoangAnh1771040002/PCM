b60a421cf68c2c47ed58f4207b823b0130803489
Nguyen Hoang Anh <157453948+NguyenHoangAnh1771040002@users.noreply.github.com>
2026-01-24 16:01:23 +0700

    Update API

--- PCM.API/Program.cs
+++ PCM.API/Program.cs
@@ -69,7 +69,8 @@ builder.Services.AddCors(options =>
 });
 
 // Add OpenAPI/Swagger
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -86,7 +87,12 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PCM API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
